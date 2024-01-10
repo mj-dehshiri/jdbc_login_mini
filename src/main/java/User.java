cd5910c81339bf097a062d169f9859c926ba9031
m.dehshiri <mj.dehshiri@gmail.com>
2024-01-10 18:41:29 +0330

    add tostring method

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -50,4 +50,14 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
