68565af53d76c0687a9394692cb9f9d6a5158624
m.dehshiri <mj.dehshiri@gmail.com>
2024-01-10 18:49:08 +0330

    create menu class

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class Menu {
+    public void publicMenu(){
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.println("***** WELCOME *****");
+        System.out.println("1- SIGN Up");
+        System.out.println("2- SIGN IN");
+        System.out.println("3- EXIT");
+        System.out.println("choose Your Number: ");
+
+        int number =scanner.nextInt();
+        scanner.nextLine();
+        System.out.println(number);
+    }
 }
