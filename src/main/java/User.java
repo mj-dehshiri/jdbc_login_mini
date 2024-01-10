3b56bbf02a1d2ec52438ffea71da3a9f83cd9c22
m.dehshiri <mj.dehshiri@gmail.com>
2024-01-10 18:40:26 +0330

    add getter and setter method for all field

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -16,4 +16,38 @@ public class User {
         this.userName = userName;
         this.password = password;
     }
+
+    //getter and setter
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
