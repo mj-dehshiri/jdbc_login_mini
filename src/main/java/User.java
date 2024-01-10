78637784badd845e6ae3972c556811d11ab66384
m.dehshiri <mj.dehshiri@gmail.com>
2024-01-10 18:38:58 +0330

    add ni args and all args constructor

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -4,4 +4,16 @@ public class User {
   private String userName;
   private String password;
 
+
+  //constructor
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String userName, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.password = password;
+    }
 }
